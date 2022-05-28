6c60ca2693883b30941de7f4107239e99acd7db8
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-05-28 23:45:11 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const url = "https://api.lwd-temp.top/api/33reply.php";
 var damoo = new Damoo('dm-screen', 'dm-canvas', 25);
 var r = "";
 var last_d = "", d = "";
+var rpool = new Map();
 
 function shoot(t) {
   damoo.play();
@@ -12,12 +13,22 @@ function gettxt() {
     method: "GET",
     url: url,
   }).then(response => {
-    d = response.data.data.replies[0].content.message;
-    if (last_d != d) {
-      shoot(d);
-      console.log(d);
-    };
-    last_d = d;
+    rplys = response.data.data.replies;
+    // Append each.rpid:each.content.message to rpool
+    for (var i = 0; i < rplys.length; i++) {
+      id = rplys[i].rpid;
+      msg = rplys[i].content.message;
+      if (rpool.get(id) == undefined) {
+        // If the rply is not in rpool, shoot it
+        shoot(msg);
+        console.log(msg);
+      }
+      rpool.set(id, msg);
+    }
+    // Delete older items in rpool if it's too big
+    if (rpool.size > 30) {
+      rpool.delete(rpool.keys().next().value);
+    }
   })
 }
 function st() {
