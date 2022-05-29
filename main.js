07ab0a8fd2e1793f9d7f31c26f0761576c4ec453
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-05-29 08:33:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 const url = "https://api.lwd-temp.top/api/33reply.php";
 var damoo = new Damoo('dm-screen', 'dm-canvas', 25);
-var r = "";
-var last_d = "", d = "";
 var rpool = new Map();
 
 function shoot(t) {
-  damoo.play();
-  damoo.emit({ text: t, color: "black" });
+    damoo.play();
+    damoo.emit({ text: t, color: "black" });
 }
+
 function gettxt() {
-  axios({
-    method: "GET",
-    url: url,
-  }).then(response => {
-    rplys = response.data.data.replies;
-    // Append each.rpid:each.content.message to rpool
-    for (var i = 0; i < rplys.length; i++) {
-      id = rplys[i].rpid;
-      msg = rplys[i].content.message;
-      if (rpool.get(id) == undefined) {
-        // If the rply is not in rpool, shoot it
-        shoot(msg);
-        console.log(msg);
-      }
-      rpool.set(id, msg);
-      // Delete older items in rpool if it's too big
-      if (rpool.size > 30) {
-        rpool.delete(rpool.keys().next().value);
-      }
-    }
-  })
+    axios({
+        method: "GET",
+        url: url,
+    }).then(response => {
+        rplys = response.data.data.replies;
+        // Append each.rpid:each.content.message to rpool
+        for (var i = 0; i < rplys.length; i++) {
+            id = rplys[i].rpid;
+            msg = rplys[i].content.message;
+            if (rpool.get(id) == undefined) {
+                // If the rply is not in rpool, shoot & save it
+                shoot(msg);
+                rpool.set(id, msg);
+                console.log(msg);
+            }
+            // Delete older items in rpool if it's too big
+            if (rpool.size > 100) {
+                rpool.delete(rpool.keys().next().value);
+            }
+        }
+    })
 }
+
 function st() {
-  window.setInterval("gettxt()", 1000);
+    window.setInterval("gettxt()", 1000);
 }
